98057b9337b24ec0161911eff0b8c039d079a741
Robin <robin888b@outlook.com>
2022-07-14 18:39:07 +0200

    small update 🙃

--- src/app.js
+++ src/app.js
@@ -1,15 +1,46 @@
 // https://e621.net/posts?limit=1&tags=order:random+fox+gay+score:%3E100
 
+function HideNSFW() {
+    const ratingDiv = document.getElementById("ratingDiv")
+    const ratingSelect = document.getElementById("rattingOption")
+    ratingSelect.value = "s"
+    ratingDiv.classList.add("dspNONE")
+}
 
-function search() {
+function ShowNSFW() {
+    const ratingDiv = document.getElementById("ratingDiv")
+    const ratingSelect = document.getElementById("rattingOption")
+    ratingDiv.classList.remove("dspNONE")
+    ratingSelect.value = "0"
+}
+
+var page = 1
+function search(arg) {
     const searchBox = document.getElementById("searchBox").value
     const rattingOption = document.getElementById("rattingOption").value
     const galerie = document.getElementById("galerie")
     const orderSelect = document.getElementById("orderSelect").value
     const minimumScore = document.getElementById("minimumScoreInput").value
-    let query = `https://e621.net/posts.json?limit=72&tags=${searchBox}+rating%3A${rattingOption}`
+    let query = `https://e621.net/posts.json?tags=${searchBox}`
+    if (rattingOption != 0){query += `+rating%3A${rattingOption}`}
     if (orderSelect == 1){query += `+order%3Arandom`}
     if (minimumScore != ""){query += `+score:%3E${minimumScore}`}
+    if (arg){
+        if (arg == 1){
+            page += 1
+        }else if (arg == -1){
+            if(page > 1){
+                page -= 1
+            }else{
+                return
+            }
+        }
+        query += `&page=${page}`
+    }else{
+        page = 1
+    }
+    
+
     console.log(query)
     const loadItem = async () => {
         const result = await fetch(query)
@@ -17,13 +48,39 @@ function search() {
         console.log(data)
         galerie.innerHTML =""
         for (let i = 0; i < data.posts.length; i++) {
-            const newItem = `<img src="${String(data.posts[i].preview.url)}" class="galerieItem" onclick="showPicture(${String(data.posts[i].id)})">`
+            let rating = ""
+            switch(data.posts[i].rating){
+                case "s" :
+                    rating = "safe"
+                break; case "q":
+                    rating = "mature"
+                break; case "e":
+                    rating = "adult"
+                break;
+            }
+            const newItem = `<a href="#top"><img src="${String(data.posts[i].preview.url)}" class="galerieItem ${rating}" onclick="showPicture(${String(data.posts[i].id)})"></a>`
             galerie.innerHTML += newItem;
         }
     }
     loadItem()
 }
 
-function showPicture(url) {
-    
+function showPicture(ID) {
+    const modalContent = document.getElementById("modalContent")
+    const loadItem = async ()=>  {
+        const result = await fetch(`https://e621.net/posts/${ID}.json`)
+        const data = await result.json()
+        console.log(data)
+        let base = `<img src="${data.post.file.url}" id="modalImg">
+        <p><span class="green">↑${data.post.score.up}</span> <span class="red">↓${data.post.score.down}</span> | Favorites: ${data.post.fav_count} | ID: ${ID} | Rating: ${data.post.rating}<br><a href="${data.post.file.url}" id="downloadPost">Download</a> | <a href="https://e621.net/posts/${ID}" id="viewOnE6">View on e621.net</a></p>`
+        modalContent.innerHTML = base
+        const modal = document.getElementById("modal")
+        modal.classList.add("active")
+    }
+    loadItem()
+}
+
+function CloseModal() {
+    const modal = document.getElementById("modal")
+    modal.classList.remove("active")
 }
